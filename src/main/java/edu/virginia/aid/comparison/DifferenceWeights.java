4e089288c43ddc73dae121f58aba7935b5f43dde
Jeff Principe <princjef@gmail.com>
2014-10-09 01:02:59 -0400

    switch to parameterized DifferenceWeights class

---
+++ src/main/java/edu/virginia/aid/comparison/DifferenceWeights.java
@@ -0,0 +1,14 @@
+package edu.virginia.aid.comparison;
+
+/**
+ * Data wrapper for weighting information in differences
+ */
+public final class DifferenceWeights {
+
+    // Variable usage
+    public static final int PARAMETER_READ = 1;
+    public static final int PARAMETER_WRITE = 2;
+    public static final int FIELD_READ = 2;
+    public static final int FIELD_WRITE = 4;
+
+}
