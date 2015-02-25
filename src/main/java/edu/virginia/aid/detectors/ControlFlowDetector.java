9ae23067cf0e6f220d1ebeebe2dbf56193ac80ff
Jeff Principe <princjef@gmail.com>
2015-02-25 01:56:38 -0500

    removed unneccessary control flow computation and associated classes

--- src/main/java/edu/virginia/aid/detectors/ControlFlowDetector.java
+++ src/main/java/edu/virginia/aid/detectors/ControlFlowDetector.java
@@ -2,12 +2,10 @@ package edu.virginia.aid.detectors;
 
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 
-import edu.virginia.aid.data.BlockProperties;
 import edu.virginia.aid.data.ExpressionInfo;
 import edu.virginia.aid.data.IdentifierName;
 import edu.virginia.aid.data.IdentifierProperties;
 import edu.virginia.aid.data.MethodFeatures;
-import edu.virginia.aid.visitors.ControlFlowVisitor;
 import edu.virginia.aid.visitors.ReturnVisitor;
 
 /**
@@ -19,14 +17,6 @@ public class ControlFlowDetector implements FeatureDetector {
 
     @Override
     public void process(MethodDeclaration method, MethodFeatures features) {
-
-        // Control flow
-        ControlFlowVisitor controlFlowVisitor = new ControlFlowVisitor(features);
-        method.accept(controlFlowVisitor);
-        BlockProperties blockProperties = new BlockProperties(features.getScope(), method.getStartPosition(),
-                method.getLength() + method.getStartPosition(), features.getSourceContext());
-        controlFlowVisitor.getConditionals().stream().forEach(conditional -> blockProperties.addConditional(conditional));
-        controlFlowVisitor.getLoops().stream().forEach(loop -> blockProperties.addLoop(loop));
 
         // Return value
         ReturnVisitor returnVisitor = new ReturnVisitor(features);
